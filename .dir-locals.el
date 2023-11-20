3e279c585a5cd82a7dbe4c47adf6a281d855fc99
Kay Freyer <kay.freyer@icloud.com>
2023-11-20 15:51:13 +0100

    Adding org-roam db

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((org-roam-directory . "/home/kay/workspace/arch-ansible-fork/org-roam-dir")
+         (org-roam-db-location . "/home/kay/workspace/arch-ansible-fork/roam-arch-ansible.db"))))
